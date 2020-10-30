3eb2d20381b9294d1d29af763ffc7bdce5299303
Kyle Soriano <soriano.christian.kyle@gmail.com>
2020-10-30 17:28:57 +0800

    Fix authenticate file not passing the phpcs standard

--- tipone_proj/app/Http/Middleware/Authenticate.php
+++ tipone_proj/app/Http/Middleware/Authenticate.php
@@ -30,7 +30,8 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param $request  \Illuminate\Http\Request
+     *
      * @return string|null
      */
     protected function redirectTo($request)
