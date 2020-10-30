59fef3846c366f77f11332a92815474e14aa8c47
Kyle <daniecah03@gmail.com>
2020-10-30 12:58:29 +0800

    Fixed routing issue for welcome page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -38,4 +38,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@
  *
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@welcome')->name('welcome');
 
 /**
  *
